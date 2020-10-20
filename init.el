fdf007ebf05175aba8dcde2ca614a2804251d8a1
Ronnie Nissan <ronnie@localhost.localdomain>
2020-10-20 08:19:15 +0300

    added some more packages and lsp support for js

--- init.el
+++ init.el
@@ -11,21 +11,32 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package use-package-ensure-system-package
+  :ensure t)
+
 ;; changing the defaults
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
 (global-hl-line-mode t)
 (global-display-line-numbers-mode)
+(setq backup-directory-alist '(("." . "~/.cache/emacssaves")))
+(defalias 'yes-or-no-p 'y-or-n-p)
+(electric-pair-mode 1)
 
 ;;;; Theming and UI
 
 ;; set fonts
-(set-frame-font "CodeNewRoman Nerd Font Mono-12" t t)
+(setq default-frame-alist `(
+			    (font . "CodeNewRoman Nerd Font Mono-12")
+			    (fullscreen . maximized)
+			    (undecorated . t)
+			    (vertical-scroll-bars . nil)
+			    (horizontal-scroll-bars . nil)
+			    ))
 
 (use-package modus-operandi-theme
-  :ensure t)
+  :disabled)
 
 (use-package modus-vivendi-theme
   :ensure t
@@ -52,7 +63,7 @@
 (use-package page-break-lines
   :ensure t
   :config
-  (turn-on-page-break-lines-mode))
+  (page-break-lines-mode))
 
 ;; Emacs Dashboard
 
@@ -108,6 +119,12 @@
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode 1))
 
+(use-package counsel-projectile
+  :ensure t
+  :after (projectile)
+  :config
+  (counsel-projectile-mode))
+
 (use-package magit
   :ensure t
   :bind
@@ -122,7 +139,7 @@
     (global-set-key (kbd "C-x o") 'ace-window)
     (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
     (setq aw-background nil)
-    ;;(setq aw-scope frame)
+    (setq aw-scope 'frame)
     (setq aw-dispatch-always t)))
 
 ;; avy
@@ -130,9 +147,149 @@
 (use-package avy
   :ensure t
   :bind
-  ("C-'" . avy-goto-char-2)
-  ("C-:" . avy-goto-line)
-  ("C-]" . avy-goto-end-of-line))
+  ("C-' '" . avy-goto-char-2)
+  ("C-' [" . avy-goto-line)
+  ("C-' ]" . avy-goto-end-of-line))
+
+(use-package expand-region
+  :ensure t
+  :bind
+  ("C-; ;" . er/expand-region)
+  ("C-; -" . er/contract-region)
+  ("C-; i p" . er/mark-inside-pairs)
+  ("C-; o p" . er/mark-outside-pairs)
+  ("C-; i q" . er/mark-inside-quotes)
+  ("C-; o q" . er/mark-outside-quotes)
+  ("C-; c" . er/mark-comment)
+  ("C-; u" . er/mark-url)
+  ("C-; e" . er/mark-email)
+  ;;("C-; s" . er/mark-sentence)
+  ;;("C-; p" . er/mark-paragraph)
+  ("C-; h a" . er/mark-html-attribute)
+  ("C-; h i" . er/mark-inner-tag)
+  ("C-; h o" . er/mark-outer-tag)
+  )
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package centaur-tabs
+  :ensure t
+  :custom
+  (centaur-tabs-style "bar")
+  (centaur-tabs-set-icons t)
+  (centaur-tabs-plain-icons t)
+  (centaur-tabs-gray-out-icons 'buffer)
+  (centaur-tabs-set-bar 'over)
+  (centaur-tabs-set-modified-marker t)
+  (centaur-tabs-modified-marker "•")
+  (centaur-tabs-cycle-scope 'tabs)
+  :config
+  (centaur-tabs-mode t)
+  :hook
+  (dired-mode . centaur-tabs-local-mode)
+  (dashboard-mode . centaur-tabs-local-mode)
+  (term-mode . centaur-tabs-local-mode)
+  (calendar-mode . centaur-tabs-local-mode)
+  (org-agenda-mode . centaur-tabs-local-mode)
+  (helpful-mode . centaur-tabs-local-mode)
+  :bind
+  ("C-x t [" . centaur-tabs-backward)
+  ("C-x t ]" . centaur-tabs-forward)
+  ("C-x t p" . centaur-tabs-group-by-projectile-project)
+  ("C-x t g" . centaur-tabs-group-buffer-groups))
+
+(use-package company
+  :ensure t
+  :diminish
+  :hook
+  (after-init . global-company-mode))
+
+(use-package company-box
+  :ensure t
+  :custom
+  (company-idle-delay 0)
+  :hook (company-mode . company-box-mode))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; for javascript
+
+(use-package rjsx-mode
+  :ensure t
+  :mode "\\.js\\'")
+
+(use-package prettier-js
+  :ensure t
+  :after (rjsx-mode)
+  :hook (rjsx-mode . prettier-js-mode))
+
+;; LSP MODE
+
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+(setq lsp-keymap-prefix "C-c l")
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((rjsx-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands
+  lsp)
+
+;; optionally
+(use-package lsp-ui
+  :ensure t
+  :commands
+  lsp-ui-mode)
+(use-package lsp-ivy
+  :ensure t
+  :commands
+  lsp-ivy-workspace-symbol)
+;;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; yasnippits setup
+
+(use-package yasnippet
+  :ensure t
+  :custom
+  (yas-snippet-dirs (append yas-snippet-dirs `("~/.emacs.d/mysnippets")))
+  :config
+  (yas-reload-all)
+  (use-package yasnippet-snippets
+    :ensure t)
+  :hook
+  (prog-mode . yas-minor-mode))
+
+(use-package emmet-mode
+  :ensure t
+  :custom
+  (emmet-expand-jsx-className? t)
+  (emmet-self-closing-tag-style " /")
+  :hook
+  (sgml-mode . emmet-mode)
+  (css-mode .  emmet-mode))
+
+(use-package vterm
+  :ensure-system-package (cmake libtool libvterm)
+  :ensure t
+  :custom
+  (vterm-copy-exclude-prompt t)
+  (vterm-always-compile-module t))
+
+(use-package multi-vterm
+  :ensure t
+  :after (vterm)
+  :bind
+  ("C-c t o" . multi-vterm)
+  ("C-c t n" . multi-vterm-next)
+  ("C-c t p" . multi-vterm-prev)
+  ("C-c t t" . multi-vterm-dedicated-toggle)
+  ("C-c t p" . multi-vterm-project))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -140,7 +297,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-vivendi-theme magit ace-window dashboard projectile ivy-rich counsel ivy page-break-lines doom-modeline modus-operandi-theme which-key use-package)))
+   '(vterm use-package-ensure-system-package emmet-mode yassnippet-snippets yasnippet-snippets react-snippets yasnippet lsp-ivy lsp-ui lsp-mode counsel-projectile prettier-js rjsx-mode flycheck company-box company centaur-tabs which-key use-package projectile org-bullets modus-vivendi-theme modus-operandi-theme magit ivy-rich expand-region doom-modeline dashboard counsel ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

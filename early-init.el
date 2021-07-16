3a7dccbdac6bc3405c1cc8d70035c7234213b797
ronnie nissan <ronni.n.yousif@gmail.com>
2021-07-16 21:07:46 +0300

    added gitignore and early-init.el

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(setq package-enable-at-startup nil)
+
+;; set fonts and frame settings
+(setq default-frame-alist `(
+			    (font . "CodeNewRoman Nerd Font Mono-12")
+			    (fullscreen . maximized)
+			    (undecorated . t)
+			    (vertical-scroll-bars . nil)
+			    (horizontal-scroll-bars . nil)
+			    ))

d5d3bf11c98db67efcc1bdc09d19a636e4e363ba
ronnie nissan <ronni.n.yousif@gmail.com>
2021-07-17 08:52:49 +0300

    better documentions and error messages with eldoc-box

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; package management
-(setq straight-use-package-by-default t)
-(setq package-enable-at-startup nil)
-(setq straight-fix-flycheck t)
+(setq straight-use-package-by-default t
+      package-enable-at-startup nil
+      straight-fix-flycheck t)
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -19,17 +19,19 @@
 (straight-use-package 'use-package)
 
 ;; changing the defaults
-(setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (global-hl-line-mode t)
 (global-display-line-numbers-mode)
-(setq backup-directory-alist '(("." . "~/.cache/emacssaves")))
 (defalias 'yes-or-no-p 'y-or-n-p)
-(electric-pair-mode 1)
-(setq create-lockfiles nil)
-(setq tab-width 2)
 (show-paren-mode 1)
+(electric-pair-mode 1)
+(setq backup-directory-alist '(("." . "~/.cache/emacssaves"))
+      inhibit-startup-message t
+      create-lockfiles nil
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil
+      tab-width 2)
 
 
 ;; eye candy
@@ -45,8 +47,8 @@
 
 (use-package echo-bell
   :config
-  (setq echo-bell-string "''")
-  (setq echo-bell-background "#000000")
+  (setq echo-bell-string "''"
+	echo-bell-background "#000000")
   :init
   (echo-bell-mode t))
 
@@ -61,14 +63,11 @@
   (savehist-mode))
 
 (use-package marginalia
-  ;;  :bind (("M-A" . marginalia-cycle)
-  ;;         :map minibuffer-local-map
-  ;;         ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
 
 (use-package orderless
-  :init
+  :config
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
@@ -76,11 +75,10 @@
 ;; usefull packages
 
 (use-package popper
-  :ensure t ; or :straight t
   :bind (("M-p"   . popper-toggle-latest)
          ("M-n"   . popper-cycle)
          ("M-`" . popper-toggle-type))
-  :init
+  :config
   (setq popper-reference-buffers
         '("\\*Messages\\*"
           "Output\\*$"
@@ -91,9 +89,8 @@
 
 ;; org-mode
 (use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  :hook
+  (org-mode-hook . (lambda () (org-bullets-mode 1))))
 
 ;; version control
 
@@ -109,44 +106,35 @@
   (after-init . global-company-mode))
 
 (use-package company-box
-  :custom
-  (company-idle-delay 0)
+  :config
+  (setq company-idle-delay 0)
   :hook (company-mode . company-box-mode))
 
 (use-package company-web)
 
-(use-package yasnippet
-  :config
-  (yas-reload-all)
-  :hook
-  (prog-mode . yas-minor-mode))
-
-(use-package yasnippet-snippets)
-
 (use-package format-all
-  :custom
-  (setq format-all-show-errors 'never)
-  (setq format-all-formatters '(
+  :config
+  (setq format-all-show-errors 'never
+	format-all-formatters '(
 				("python" "black")
 				("javascript" "prettier")
 				("vuejs" "prettier")
 				))
   :hook
   (format-all-mode . format-all-ensure-formatter)
-  (prog-mode . format-all-mode)
-  (before-save . format-all-mode))
+  ((prog-mode before-save) . format-all-mode))
 
 ;; web-stuff
 (use-package web-mode
   :mode
   ("\\.css?\\'" . web-mode)
   ("\\.html?\\'" . web-mode)
-  :init
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-enable-current-column-highlight t)
-  (setq web-mode-enable-current-element-highlight t)
+  :config
+  (setq web-mode-markup-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2
+	web-mode-enable-current-column-highlight t
+	web-mode-enable-current-element-highlight t)
   (set (make-local-variable 'company-backends) '(company-css company-web-html company-yasnippet company-files)))
 
 (define-derived-mode my-vue-mode web-mode "vueMode"
@@ -158,17 +146,14 @@
   ("\\.vue\\'" . my-vue-mode))
 
 (use-package emmet-mode
-  :custom
-  (emmet-self-closing-tag-style " /")
+  :config
+  (setq emmet-self-closing-tag-style " /")
   :hook
-  (web-mode . emmet-mode)
-  (my-vue-mode . emmet-mode)
-  (rjsx-mode . emmet-mode))
+  ((web-mode my-vue-mode rjsx-mode) . emmet-mode))
 
 ;; javascript
 
 (use-package rjsx-mode
-  :ensure t
   :mode
   ("\\.js\\'" . rjsx-mode)
   ("\\.jsx\\'" . rjsx-mode))
@@ -176,7 +161,7 @@
 ;; rust
 
 (use-package rust-mode
-  :custom
+  :config
   (setq rust-format-on-save t)
   :hook
   (rust-mode . (lambda () (setq indent-tabs-mode nil))))
@@ -186,9 +171,24 @@
   :config
   (add-to-list 'eglot-server-programs '(my-vue-mode "vls"))
   (add-to-list 'eglot-server-programs '(rust-mode "rust-analyzer"))
-  (add-hook 'my-vue-mode-hook #'eglot-ensure)
-  (add-hook 'rjsx-mode-hook #'eglot-ensure)
-  (add-hook 'python-mode #'eglot-ensure))
+  (add-to-list 'eglot-ignored-server-capabilites :hoverProvider)
+  :hook
+  ((my-vue-mode rjsx-mode python-mode) . eglot-ensure))
+
+(use-package eldoc-box
+  :after eldoc
+  :config
+  (eldoc-box-hover-mode))
+
+(defconst emacs-start-time (current-time))
+
+(add-hook 'after-init-hook
+          `(lambda ()
+             (let ((elapsed
+                    (float-time
+                     (time-subtract (current-time) emacs-start-time))))
+               (message "Loading %s...done (%.2fs) [after-init]"
+                        ,load-file-name elapsed))) t)
 
 (provide 'init)
 ;;; init ends here
